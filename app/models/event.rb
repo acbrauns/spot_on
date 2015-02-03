75d5b6be53de54cd5acada5ba319ac86433bd96b
Andrea Braunstein <acbraunstein@gmail.com>
2015-02-03 06:13:55 -0800

    validates for events

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,3 @@
 class Event < ActiveRecord::Base
-
-
+  validates_presence_of :title, :start_date, :end_date, :start_time, :end_time, :location
 end
-
-
--- public/js/application.js
+++ public/js/application.js
@@ -7,29 +7,20 @@ $(document).ready(function () {
       type: 'POST',
       data: $(this).serialize()
     }).done(function(response){
-      $('.events_list').prepend('<tr class="top_row"><td><p>' + response.day + '</p></td><td><p>'
-        + response.start_time + ' - ' + response.end_time + '</p></td><tr class="bottom_row"><td><p>' +
-        response.month_day + '</p></td><td><p class="event_title">' + response.title + '</p></td></tr>');
+      if(response.saved==true){
+        $('.events_list').prepend('<tr class="top_row"><td><p>' + response.day + '</p></td><td><p>'
+          + response.start_time + ' - ' + response.end_time + '</p></td><tr class="bottom_row"><td><p>' +
+          response.month_day + '</p></td><td><p class="event_title">' + response.title + '</p></td></tr>');
 
-      $('.grid_view').prepend('<div class="col-xs-3 grid-container"><header class="tile_header"><p><b>' + response.day + ', ' + response.hyphen_date +
-        '<br>' + response.start_time + '</b></p></header><div class="tile_text"><p><b>' + response.title +
-        '</b></p><p>' + response.location + '</p></div></div>');
+        $('.grid_view').prepend('<div class="col-xs-3 grid-container"><header class="tile_header"><p><b>' + response.day
+          + ', ' + response.hyphen_date +
+          '<br>' + response.start_time + '</b></p></header><div class="tile_text"><p><b>' + response.title +
+          '</b></p><p>' + response.location + '</p></div></div>');
 
-
-
-// <header class="tile_header">
-//           <p><b><%= (event.start_date).strftime("%a")%>, <%= (event.start_date).strftime("%m-%d")%><br>
-//             <%= (event.start_time).strftime("%I:%M %p") %>
-//           </b></p>
-//         </header>
-//         <div class="tile_text">
-//           <p><b><%= event.title %></b></p>
-//           <p><%= event.location %></p>
-//         </div>
-//       </div>
-
-
-      $('form.create_event').find("input[type=text], input[type=date], input[type=time]").val("");
+        $('form.create_event').find("input[type=text], input[type=date], input[type=time]").val("");
+      }
+    }).fail(function(){
+      console.log("fail");
     });
   });
 
@@ -40,6 +31,8 @@ $(document).ready(function () {
     e.preventDefault();
     $('.list_view').show();
     $('.grid_view').hide();
+    $(this).css("color", "white");
+    $('.grid_toggle').css("color", "#819EB2");
   });
 
 
@@ -47,6 +40,9 @@ $(document).ready(function () {
     e.preventDefault();
     $('.list_view').hide();
     $('.grid_view').show();
+    $(this).css("color", "white");
+    $('.list_toggle').css("color", "#819EB2");
+
   });
 
 });
