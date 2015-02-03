7e567a3404c28c9f22d8f53d8254e1c2d2bb9ff6
Andrea Braunstein <acbraunstein@gmail.com>
2015-02-03 01:48:05 -0800

    prepends view correctly for list

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -9,6 +9,9 @@ end
 
 post '/events/create' do
   event = Event.create(params)
+  day = (event.start_date).strftime("%a")
+  start_time = (event.start_time).strftime("%I:%M %p")
+  month_day = (event.start_date).strftime("%d")
   content_type :json
-  {event: event}.to_json
+  {title: event.title, day: day, start_time: start_time, month_day: month_day}.to_json
 end
--- public/js/application.js
+++ public/js/application.js
@@ -8,14 +8,18 @@ $(document).ready(function () {
       type: 'POST',
       data: $(this).serialize()
     }).done(function(response){
-      event = response.event;
-      $('.events_list').prepend('<li>' + event.title + '</li>');
-      $('form.create_event').find("input[type=text], input[type=date]").val("");
+      // event = response.event;
+      $('.events_list').prepend('<tr><td>' + response.day + '</td><td>'
+        + response.start_time + '</td><tr class="bottom_line"><td>' +
+        response.month_day + '</td><td>' + response.title + '</td></tr>');
+
+      $('form.create_event').find("input[type=text], input[type=date], input[type=time]").val("");
     });
   });
 
 
-  //for persistence w/ layout on page reload, use helper method/boolean instead?
+  //defaults to list view
+  //for persistence w/ layout on page reload, use helper method/toggle boolean instead?
   $(".list_toggle").on("click", function(e){
     e.preventDefault();
     $('.list_view').show();
