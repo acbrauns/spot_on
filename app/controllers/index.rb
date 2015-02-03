94ed76c52c34996809a35c525f215f0e99e8c42f
Andrea Braunstein <acbraunstein@gmail.com>
2015-02-03 05:42:44 -0800

    view tinkering

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,4 +1,3 @@
-require 'pry'
 
 get '/' do
   @events = Event.all
@@ -8,9 +7,10 @@ end
 post '/events/create' do
   event = Event.create(params)
   day = (event.start_date).strftime("%a")
+  hyphen_date = (event.start_date).strftime("%m-%d")
   start_time = (event.start_time).strftime("%I:%M %p")
   end_time = (event.end_time).strftime("%I:%M %p")
   month_day = (event.start_date).strftime("%d")
   content_type :json
-  {title: event.title, day: day, start_time: start_time, end_time: end_time, month_day: month_day}.to_json
+  {title: event.title, day: day, start_time: start_time, end_time: end_time, month_day: month_day, hyphen_date: hyphen_date, location: event.location}.to_json
 end
--- public/js/application.js
+++ public/js/application.js
@@ -11,6 +11,24 @@ $(document).ready(function () {
         + response.start_time + ' - ' + response.end_time + '</p></td><tr class="bottom_row"><td><p>' +
         response.month_day + '</p></td><td><p class="event_title">' + response.title + '</p></td></tr>');
 
+      $('.grid_view').prepend('<div class="col-xs-3 grid-container"><header class="tile_header"><p><b>' + response.day + ', ' + response.hyphen_date +
+        '<br>' + response.start_time + '</b></p></header><div class="tile_text"><p><b>' + response.title +
+        '</b></p><p>' + response.location + '</p></div></div>');
+
+
+
+// <header class="tile_header">
+//           <p><b><%= (event.start_date).strftime("%a")%>, <%= (event.start_date).strftime("%m-%d")%><br>
+//             <%= (event.start_time).strftime("%I:%M %p") %>
+//           </b></p>
+//         </header>
+//         <div class="tile_text">
+//           <p><b><%= event.title %></b></p>
+//           <p><%= event.location %></p>
+//         </div>
+//       </div>
+
+
       $('form.create_event').find("input[type=text], input[type=date], input[type=time]").val("");
     });
   });
